636975b9ccce130ced536704d520f2071cc350c4
audgks5551 <audgks5551@daum.net>
2022-02-08 10:17:22 +0900

    파일 정리 및 리액트 기본 구조 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    isLoading: true,
+    movie: []
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
+  }
+
+  render() {
+
+    const { isLoading } = this.state;
+    return (
+      <div>
+        { isLoading ? "Loading..." : "We are Ready" }
+      </div>
+    );
+
+  }
 }
 
 export default App;

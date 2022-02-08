af5b6778bf6262ea74cce4ae96960b711e93a37b
audgks5551 <audgks5551@daum.net>
2022-02-08 10:22:02 +0900

    주석 추가

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import React from "react";
 
 class App extends React.Component {
 
+  // state는 변경해야 하는 데이터들을 모아두는 곳
   state = {
     isLoading: true,
     movie: []
   }
 
+  // 위 함수는 React.Component에서 가져온 함수이며 처음에 마운트되었을 때 실행되는 함수이다. 
   componentDidMount() {
     setTimeout(() => {
       this.setState({ isLoading: false });
     }, 3000);
-  }
+  } // setState를 사용하여 isLoading을 false로 변경(setState는 state를 수정 후 render()를 실행한다.)
 
   render() {
 
-    const { isLoading } = this.state;
+    const { isLoading } = this.state; // 이 변수는 this.state.isloding과 같다
     return (
       <div>
         { isLoading ? "Loading..." : "We are Ready" }
